7479e03413651786882f0bba2e712bf74d38a54a
caijynb <1332794849@qq.com>
2021-11-06 20:06:19 +0800

    Install gitbook plugins

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ files=$(ls -I "book.json" -I "node_modules")
 for file in $files
 do
   cd "$srcpath/$file"
+  cp "$rootpath/book.json" book.json
   gitbook build
   mv _book "$rootpath/${file}"
   echo "-----${file}文档编译部署成功-----"
